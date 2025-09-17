3e11da1b1ee77489be6475c93d5794ca66c792b5
siddhant <siddhantkumarupmanyu@gmail.com>
2025-09-18 02:47:42 +0530

    http2 via programmatically

--- src/main/java/com/myeglu/stopgap/Main.java
+++ src/main/java/com/myeglu/stopgap/Main.java
@@ -2,6 +2,10 @@ package com.myeglu.stopgap;
 
 import io.helidon.config.Config;
 import io.helidon.webserver.WebServer;
+import io.helidon.webserver.WebServerConfig;
+import io.helidon.webserver.http2.Http2Config;
+
+import java.util.List;
 
 public final class Main {
 
@@ -9,15 +13,19 @@ public final class Main {
     }
 
     public static void main(String[] args) {
-
         final var config = Config.create();
 
-        final var serverConfig = config.get("server");
+        final var http2Config = Http2Config.builder().build();
 
         final var server = WebServer.builder()
-                .config(serverConfig)
+                .config(config.get("server"))
+                .protocolsDiscoverServices(false)
+                .protocols(List.of(http2Config))
                 .build();
         server.start();
+
+        WebServerConfig serverConfig = server.prototype();
+        System.out.println("Protocols: " + serverConfig.protocols());
     }
 
 }
