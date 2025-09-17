a8e23fc5548a443f4fb075b8dbab531c01ff9d82
siddhant <siddhantkumarupmanyu@gmail.com>
2025-09-18 00:51:24 +0530

    Integrate Helidon Config to enable YAML-based configuration and set up basic server configuration in `application.yaml`

--- src/main/java/com/myeglu/stopgap/Main.java
+++ src/main/java/com/myeglu/stopgap/Main.java
@@ -1,9 +1,25 @@
 package com.myeglu.stopgap;
 
-public class Main {
+import io.helidon.config.Config;
+import io.helidon.webserver.WebServer;
+
+public final class Main {
+
+    private Main() {
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        final var config = Config.create();
+
+        final var serverConfig = config.get("server");
+
+        final var server = WebServer.builder()
+                .config(serverConfig)
+                .build();
+        server.start();
+
+        System.out.println("Server started at http://:" + server + server.port());
     }
 
 }
