2467f54bdc92af148aea02ba54939be5e0a45fa3
siddhant <siddhantkumarupmanyu@gmail.com>
2025-09-18 03:04:28 +0530

    reading from application.yaml but http_1_1 also gets
    - this cause of discovery

--- src/main/java/com/myeglu/stopgap/Main.java
+++ src/main/java/com/myeglu/stopgap/Main.java
@@ -3,9 +3,6 @@ package com.myeglu.stopgap;
 import io.helidon.config.Config;
 import io.helidon.webserver.WebServer;
 import io.helidon.webserver.WebServerConfig;
-import io.helidon.webserver.http2.Http2Config;
-
-import java.util.List;
 
 public final class Main {
 
@@ -15,12 +12,8 @@ public final class Main {
     public static void main(String[] args) {
         final var config = Config.create();
 
-        final var http2Config = Http2Config.builder().build();
-
         final var server = WebServer.builder()
                 .config(config.get("server"))
-                .protocolsDiscoverServices(false)
-                .protocols(List.of(http2Config))
                 .build();
         server.start();
 
