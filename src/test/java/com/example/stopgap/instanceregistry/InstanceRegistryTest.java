cc347a9054301c2d1ec93aef21bf1488a63b9491
siddhant <siddhantkumarupmanyu@gmail.com>
2025-09-20 04:03:56 +0530

    Refactor `InstanceRegistryTest` to replace `TestClass` with `T1` and simplify instance creation logic

--- src/test/java/com/example/stopgap/instanceregistry/InstanceRegistryTest.java
+++ src/test/java/com/example/stopgap/instanceregistry/InstanceRegistryTest.java
@@ -11,19 +11,19 @@ final class InstanceRegistryTest {
 
     @Test
     void returnsSameInstance() {
-        final var qualifier = TestClass.class.getSimpleName();
+        final var qualifier = T1.class.getSimpleName();
         registry.register(qualifier, InstanceRegistryTest::createTestInstance);
 
-        final var i1 = registry.getInstance(qualifier, TestClass.class);
-        final var i2 = registry.getInstance(qualifier, TestClass.class);
+        final var i1 = registry.getInstance(qualifier, T1.class);
+        final var i2 = registry.getInstance(qualifier, T1.class);
 
-        assertThat(i1).isInstanceOf(TestClass.class);
+        assertThat(i1).isInstanceOf(T1.class);
         assertThat(i1).isSameAs(i2);
     }
 
     @Test
     void onlyRegisterOnce() {
-        final var qualifier = TestClass.class.getSimpleName();
+        final var qualifier = T1.class.getSimpleName();
         registry.register(qualifier, InstanceRegistryTest::createTestInstance);
         assertThatExceptionOfType(CreatorExistsException.class).isThrownBy(() -> {
             registry.register(qualifier, InstanceRegistryTest::createTestInstance);
@@ -37,24 +37,23 @@ final class InstanceRegistryTest {
 
         final var t2 = registry.getInstance("t2", T2.class);
         assertThat(t2).isInstanceOf(T2.class);
-        final var t1 = registry.getInstance("t1", TestClass.class);
+        final var t1 = registry.getInstance("t1", T1.class);
         assertThat(t1).isSameAs(t2.t1);
     }
 
-    private static TestClass createTestInstance(final InstanceRegistry registry) {
-        return new TestClass();
+    private static T1 createTestInstance(final InstanceRegistry registry) {
+        return new T1();
     }
 
-    @SuppressWarnings("EmptyClass")
-    private static class TestClass {
+    private record T1() {
     }
 
     private static T2 createT2(final InstanceRegistry registry) {
-        final var t1 = registry.getInstance("t1", TestClass.class);
+        final var t1 = registry.getInstance("t1", T1.class);
         return new T2(t1);
     }
 
-    private record T2(TestClass t1) {
+    private record T2(T1 t1) {
     }
 
 }
