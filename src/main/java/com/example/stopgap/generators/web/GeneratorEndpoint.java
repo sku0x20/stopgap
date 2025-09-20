9230d6c430bcb9138cea4ce0c6b0c0ba9d1789d4
siddhant <siddhantkumarupmanyu@gmail.com>
2025-09-20 22:35:22 +0530

    Add `GeneratorEndpoint` with routing logic for random number generation

---
+++ src/main/java/com/example/stopgap/generators/web/GeneratorEndpoint.java
@@ -0,0 +1,28 @@
+package com.example.stopgap.generators.web;
+
+import com.example.stopgap.Endpoint;
+import io.helidon.webserver.http.HttpRules;
+import io.helidon.webserver.http.HttpService;
+import io.helidon.webserver.http.ServerRequest;
+import io.helidon.webserver.http.ServerResponse;
+
+import java.security.SecureRandom;
+
+public final class GeneratorEndpoint implements Endpoint {
+
+    private final SecureRandom random = new SecureRandom();
+
+    @Override
+    public HttpService routes() {
+        return (final HttpRules rules) -> rules
+            .get("/number", this::randomNumber);
+    }
+
+    private void randomNumber(
+        final ServerRequest req,
+        final ServerResponse res
+    ) {
+        res.send(random.nextLong());
+    }
+
+}
