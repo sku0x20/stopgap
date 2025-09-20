5416cf3363a001d23df65a70eb5764c77b0c7ae6
siddhant <siddhantkumarupmanyu@gmail.com>
2025-09-20 22:01:54 +0530

    wip nested routing

--- src/main/java/com/example/stopgap/Main.java
+++ src/main/java/com/example/stopgap/Main.java
@@ -3,22 +3,37 @@ package com.example.stopgap;
 import io.helidon.config.Config;
 import io.helidon.webserver.WebServer;
 import io.helidon.webserver.WebServerConfig;
+import io.helidon.webserver.http.HttpRouting;
+import io.helidon.webserver.http.HttpRules;
+import io.helidon.webserver.http.HttpService;
 
 public final class Main {
 
     private Main() {
     }
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         final var config = Config.create();
 
+        final var r2 = new HttpService() {
+            @Override
+            public void routing(final HttpRules rules) {
+                rules.get("/i", (req, res) -> res.send("2nd"));
+            }
+        };
+
+        final var routing = HttpRouting.builder()
+            .get("/hello", (req, res) -> res.send("Hello World!"))
+            .register("/inner", r2);
+
         final var server = WebServer.builder()
-                .config(config.get("server"))
-                .protocolsDiscoverServices(false)
-                .build();
+            .config(config.get("server"))
+            .protocolsDiscoverServices(false)
+            .routing(routing)
+            .build();
         server.start();
 
-        WebServerConfig serverConfig = server.prototype();
+        final WebServerConfig serverConfig = server.prototype();
         System.out.println("Protocols: " + serverConfig.protocols());
     }
 
