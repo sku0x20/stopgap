06df5806e0813105adf9cd8634fa62db728cf0fd
siddhant <siddhantkumarupmanyu@gmail.com>
2025-09-21 23:35:22 +0530

    Refine comment in `BadClientException` to clarify design choice over error handler registration.

--- src/main/java/com/example/stopgap/exception/BadClientException.java
+++ src/main/java/com/example/stopgap/exception/BadClientException.java
@@ -3,6 +3,7 @@ package com.example.stopgap.exception;
 import io.helidon.http.HttpException;
 import io.helidon.http.Status;
 
+// i like this more than registering an error handler in the webserver
 public class BadClientException extends HttpException {
     public BadClientException() {
         super("Bad client request by extending", Status.BAD_REQUEST_400);
