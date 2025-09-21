7621cb53df0bdbdf25f7deffead71624375ca5aa
siddhant <siddhantkumarupmanyu@gmail.com>
2025-09-21 18:52:06 +0530

    Add documentation to `Config` interface in `InstanceRegistry` outlining future extensibility plans.

--- src/main/java/com/example/stopgap/instanceregistry/Config.java
+++ src/main/java/com/example/stopgap/instanceregistry/Config.java
@@ -1,6 +1,10 @@
 package com.example.stopgap.instanceregistry;
 
 
+/**
+ * it follows property syntax for now.
+ * will be later extended to be more generic.
+ */
 public interface Config {
     String get(String key);
 }
