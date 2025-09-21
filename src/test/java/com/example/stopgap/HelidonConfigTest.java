ce3d0acc479ded35b8ef904baa4245280a3e9545
siddhant <siddhantkumarupmanyu@gmail.com>
2025-09-21 19:19:15 +0530

    Update `HelidonConfigTest` to use `ConfigNode.ObjectNode` for hierarchical configuration testing.

--- src/test/java/com/example/stopgap/HelidonConfigTest.java
+++ src/test/java/com/example/stopgap/HelidonConfigTest.java
@@ -2,22 +2,27 @@ package com.example.stopgap;
 
 import io.helidon.config.Config;
 import io.helidon.config.ConfigSources;
+import io.helidon.config.spi.ConfigNode;
 import org.junit.jupiter.api.Test;
-
-import java.util.Map;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 final class HelidonConfigTest {
 
-    @SuppressWarnings("DuplicateStringLiteralInspection")
     @Test
     void get() {
-        final var configSource = ConfigSources.create(Map.of("test", "some-value"));
+        final var objectNode = ConfigNode.ObjectNode.builder()
+            .addValue("a", "a")
+            .addObject("b", ConfigNode.ObjectNode.builder()
+                .addValue("b1", "b1")
+                .build())
+            .build();
+        final var configSource = ConfigSources.create(objectNode);
         final var source = Config.create(configSource);
+
         final var config = new HelidonConfig(source);
-        final var value = config.get("test");
-        assertThat(value).isEqualTo("some-value");
+        assertThat(config.get("a")).isEqualTo("a");
+        assertThat(config.get("b.b1")).isEqualTo("b1");
     }
 
 }
