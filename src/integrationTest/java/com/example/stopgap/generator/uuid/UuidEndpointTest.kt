5d7572ec1ec33173433aa043e211a7683624920f
siddhant <siddhantkumarupmanyu@gmail.com>
2025-09-25 02:43:03 +0530

    Migrate `UuidEndpointTest` from Java to Kotlin. Rename `.java` to `.kt`.

--- src/integrationTest/java/com/example/stopgap/generator/uuid/UuidEndpointTest.kt
+++ src/integrationTest/java/com/example/stopgap/generator/uuid/UuidEndpointTest.kt
@@ -1,42 +1,38 @@
-package com.example.stopgap.generator.uuid;
+package com.example.stopgap.generator.uuid
 
-import com.example.stopgap.generator.uuid.web.UuidEndpoint;
-import com.example.stopgap.instanceregistry.Config;
-import com.example.stopgap.instanceregistry.InstanceRegistry;
-import io.helidon.http.Status;
-import io.helidon.webclient.http1.Http1Client;
-import io.helidon.webserver.http.HttpRouting;
-import io.helidon.webserver.testing.junit5.RoutingTest;
-import io.helidon.webserver.testing.junit5.SetUpRoute;
-import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.Mockito.mock;
+import com.example.stopgap.generator.uuid.web.UuidEndpoint
+import com.example.stopgap.instanceregistry.Config
+import com.example.stopgap.instanceregistry.InstanceRegistry
+import io.helidon.http.Status
+import io.helidon.webclient.http1.Http1Client
+import io.helidon.webserver.http.HttpRouting
+import io.helidon.webserver.testing.junit5.RoutingTest
+import io.helidon.webserver.testing.junit5.SetUpRoute
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito.mock
 
 @RoutingTest
-public final class UuidEndpointTest {
-
-    private static final Config config = mock(Config.class);
-    private static final InstanceRegistry registry = new InstanceRegistry(config);
-
-    private static final UuidEndpoint endpoint = new UuidEndpoint(new UuidGen());
-
-    private final Http1Client client;
-
-    public UuidEndpointTest(Http1Client client) {
-        this.client = client;
-    }
-
-    @SetUpRoute
-    static void setUpRoute(final HttpRouting.Builder builder) {
-        builder.register(endpoint.routes(registry));
-    }
+class UuidEndpointTest(
+    private val client: Http1Client
+) {
 
     @Test
-    void returnsUuid() {
-        try (final var response = client.get("/").request()) {
-            assertThat(response.status()).isEqualTo(Status.OK_200);
+    fun returnsUuid() {
+        client.get("/").request().use { response ->
+            assertThat(response.status()).isEqualTo(Status.OK_200)
         }
     }
 
+    companion object {
+        private val config: Config = mock<Config>(Config::class.java)
+        private val registry = InstanceRegistry(config)
+
+        private val endpoint = UuidEndpoint(UuidGen())
+
+        @SetUpRoute
+        fun setUpRoute(builder: HttpRouting.Builder) {
+            builder.register(endpoint.routes(registry))
+        }
+    }
 }
