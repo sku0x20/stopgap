9c8301cdc8bd9db7f04f7abad1fa3914d5782ecf
siddhant <siddhantkumarupmanyu@gmail.com>
2025-11-09 13:27:43 +0530

    Add integration test for exception handling in `ExceptionEndpoint`.

---
+++ src/integrationTest/kotlin/com/example/stopgap/exception/ExceptionEndpointTest.kt
@@ -0,0 +1,33 @@
+package com.example.stopgap.exception
+
+import com.example.stopgap.instanceregistry.InstanceRegistry
+import extension.InjectInstance
+import extension.WebserverTest
+import io.helidon.http.Status
+import io.helidon.webclient.api.WebClient
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+
+@WebserverTest
+class ExceptionEndpointTest {
+
+    @InjectInstance
+    lateinit var client: WebClient
+
+    @Test
+    fun badClient1() {
+        val response = client.get("/bad-client-1").request()
+        assertThat(response.status()).isEqualTo(Status.BAD_REQUEST_400)
+    }
+
+    companion object {
+
+        @JvmStatic
+        @WebserverTest.SetupInstanceRegistry(ExceptionEndpoint::class)
+        fun setupInstanceRegistry(registry: InstanceRegistry) {
+            registry.registerForType { ExceptionEndpoint() }
+        }
+    }
+
+}
