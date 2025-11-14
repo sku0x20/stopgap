eaa55cf602f6e9257b8a87c14aaf4c89f5ada694
siddhant <siddhantkumarupmanyu@gmail.com>
2025-11-15 00:08:17 +0530

    Add Passing test

---
+++ src/e2eTest/kotlin/e2e/Sample.kt
@@ -0,0 +1,13 @@
+package e2e
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class Sample {
+
+    @Test
+    fun pass() {
+        assertThat(true).isTrue()
+    }
+
+}
