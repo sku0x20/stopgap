7851aa2df530299f84f5f1f1af234b279552d7a3
siddhant <siddhantkumarupmanyu@gmail.com>
2025-11-16 13:53:27 +0530

    Introduce `HelidonWebClient` for seamless integration with Helidon WebClient in e2e tests.

---
+++ src/e2eTest/kotlin/clients/HelidonWebClient.kt
@@ -0,0 +1,19 @@
+package clients
+
+import extension.webserverclient.WebserverClient
+import io.helidon.webclient.api.WebClient
+
+class HelidonWebClient : WebserverClient<WebClient> {
+
+    override fun create(host: String, port: Int): WebClient {
+        val client = WebClient.builder()
+            .baseUri("http://${host}:${port}")
+            .build()
+        return client
+    }
+
+    override fun close(client: WebClient) {
+        client.closeResource()
+    }
+
+}
