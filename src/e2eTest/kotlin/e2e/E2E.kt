410050aeed726b65dcdef91ea59da32e43287e64
siddhant <siddhantkumarupmanyu@gmail.com>
2025-11-16 14:26:00 +0530

    Inject `WebClient` in e2e test using `InjectInstance` annotation and remove manual client setup.

--- src/e2eTest/kotlin/e2e/E2E.kt
+++ src/e2eTest/kotlin/e2e/E2E.kt
@@ -1,5 +1,6 @@
 package e2e
 
+import extension.InjectInstance
 import extension.webserverclient.ClientExtension
 import io.helidon.http.Status
 import io.helidon.webclient.api.WebClient
@@ -10,19 +11,14 @@ import org.junit.jupiter.api.extension.ExtendWith
 @ExtendWith(ClientExtension::class)
 class E2E {
 
+    @InjectInstance
+    lateinit var client: WebClient
+
     @Test
     fun ping() {
-        val port = System.getProperty("container.server.port")
-
-        val client = WebClient.builder()
-            .baseUri("http://localhost:${port}")
-            .build()
-
         val response = client.get("/ping").request()
         assertThat(response.status()).isEqualTo(Status.OK_200)
         assertThat(response.`as`(String::class.java)).isEqualTo("pong")
-
-        client.closeResource()
     }
 
 }
