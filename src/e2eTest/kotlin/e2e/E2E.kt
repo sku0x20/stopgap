811c9619ff99c9596bf8fceaaa56762818caefec
siddhant <siddhantkumarupmanyu@gmail.com>
2025-11-16 22:49:00 +0530

    Add new E2E tests for exception handling, UUID generation, number generation, and 404 responses

--- src/e2eTest/kotlin/e2e/E2E.kt
+++ src/e2eTest/kotlin/e2e/E2E.kt
@@ -18,4 +18,35 @@ class E2E {
         assertThat(response.`as`(String::class.java)).isEqualTo("pong")
     }
 
+    @Test
+    fun exceptions() {
+        val response = client.get("/exceptions/bad-client-1").request()
+        assertThat(response.status()).isEqualTo(Status.BAD_REQUEST_400)
+
+        val response2 = client.get("/exceptions/bad-client-2").request()
+        assertThat(response2.status()).isEqualTo(Status.BAD_REQUEST_400)
+    }
+
+    @Test
+    fun generator() {
+        val response = client.get("/generate/uuid").request()
+        assertThat(response.status()).isEqualTo(Status.OK_200)
+        assertThat(response.`as`(String::class.java))
+            .matches("[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}")
+
+        val response2 = client.get("/generate/number").request()
+        assertThat(response2.status()).isEqualTo(Status.OK_200)
+        assertThat(response2.`as`(String::class.java)).matches("\\d+")
+
+        val response3 = client.get("/generate/static").request()
+        assertThat(response3.status()).isEqualTo(Status.OK_200)
+        assertThat(response3.`as`(String::class.java)).isEqualTo("from static generator")
+    }
+
+    @Test
+    fun notFound() {
+        val response = client.get("/not-found").request()
+        assertThat(response.status()).isEqualTo(Status.NOT_FOUND_404)
+    }
+
 }
