f8efbe4768f03777d1cb03e77969c72874d3af63
siddhant <siddhantkumarupmanyu@gmail.com>
2025-11-26 01:31:35 +0530

    Migrate `build.gradle.kts` to use `libs.versions.toml` for dependency version management.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
-    kotlin("jvm") version "2.3.0-Beta2"
+    alias(libs.plugins.kotlin.jvm)
     application
 }
 
