f601b20cf2aa753512a952971ed669f0946a286f
siddhant <siddhantkumarupmanyu@gmail.com>
2025-11-26 01:41:50 +0530

    Migrate AssertJ dependency to `libs.versions.toml` for centralized version management.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ val junitVersion = "6.0.1"
 dependencies {
     implementation(libs.bundles.helidon)
 
-    testImplementation("org.assertj:assertj-core:3.27.3")
+    testImplementation(libs.assertj.core)
     testImplementation("org.mockito.kotlin:mockito-kotlin:6.1.0")
 
     testImplementation(libs.helidon.webclient)
