eeb56e8e73491a55ee2d3d70e373783c1cc81397
siddhant <siddhantkumarupmanyu@gmail.com>
2025-12-23 02:13:48 +0530

    Exclude `ksp.api` from `runtimeElements` configuration in `ir/build.gradle.kts`.

--- ir/build.gradle.kts
+++ ir/build.gradle.kts
@@ -9,3 +9,10 @@ dependencies {
     implementation(libs.ksp.api)
     testImplementation(libs.assertj.core)
 }
+
+configurations {
+    val kspApi = libs.ksp.api.get()
+    runtimeElements {
+        exclude(group = kspApi.group, module = kspApi.name)
+    }
+}
