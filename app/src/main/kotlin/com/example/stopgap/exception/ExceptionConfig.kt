6e892f88c94a0eafb4eea20585d28b759cf50609
siddhant <siddhantkumarupmanyu@gmail.com>
2025-12-24 00:55:01 +0530

    Add `@Creates` annotations to all configs

--- app/src/main/kotlin/com/example/stopgap/exception/ExceptionConfig.kt
+++ app/src/main/kotlin/com/example/stopgap/exception/ExceptionConfig.kt
@@ -1,9 +1,11 @@
 package com.example.stopgap.exception
 
+import dev.sku20.ir.Creates
 import dev.sku20.ir.InstanceRegistry
 
 object ExceptionConfig {
 
+    @Creates
     fun exceptionEndpoint(registry: InstanceRegistry): ExceptionEndpoint {
         return ExceptionEndpoint()
     }
--- app/src/main/kotlin/com/example/stopgap/generator/uuid/UuidConfig.kt
+++ app/src/main/kotlin/com/example/stopgap/generator/uuid/UuidConfig.kt
@@ -1,15 +1,18 @@
 package com.example.stopgap.generator.uuid
 
 import com.example.stopgap.generator.uuid.web.UuidEndpoint
+import dev.sku20.ir.Creates
 import dev.sku20.ir.InstanceRegistry
 
 object UuidConfig {
 
+    @Creates
     fun uuidEndpoint(registry: InstanceRegistry): UuidEndpoint {
         val uuidGen = registry.getInstanceForType<UuidGen>()
         return UuidEndpoint(uuidGen)
     }
 
+    @Creates
     fun uuidGen(registry: InstanceRegistry): UuidGen {
         return UuidGen()
     }
