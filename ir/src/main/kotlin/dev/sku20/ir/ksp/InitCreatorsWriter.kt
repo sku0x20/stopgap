efe1c371a3fa908c1e52652dd3e6603482e818dd
siddhant <siddhantkumarupmanyu@gmail.com>
2025-12-24 00:47:35 +0530

    Refactor `InitCreatorsWriter` to introduce indentation management and improve code clarity.

--- ir/src/main/kotlin/dev/sku20/ir/ksp/InitCreatorsWriter.kt
+++ ir/src/main/kotlin/dev/sku20/ir/ksp/InitCreatorsWriter.kt
@@ -13,27 +13,55 @@ class InitCreatorsWriter(
 
     fun write() {
         writeHeader()
-        symbols.forEach { symbol ->
-            writer.appendLine("registry.registerForType {")
-            writer.appendLine("${symbol.qualifiedName!!.asString()}(registry)")
-            writer.appendLine("}")
+        for (symbol in symbols) {
+            setIndent(8)
+            writeLine("registry.registerForType {")
+            setIndent(12)
+            writeLine("${symbol.qualifiedName!!.asString()}(registry)")
+            setIndent(8)
+            writeLine("}")
         }
         writeFooter()
         writer.close()
     }
 
     private fun writeHeader() {
-        writer.appendLine("package $packageName")
-        writer.appendLine()
-        writer.appendLine("import dev.sku20.ir.InstanceRegistry")
-        writer.appendLine()
-        writer.appendLine("object $fileName {")
-        writer.appendLine("fun init(registry: InstanceRegistry) {")
+        setIndent(0)
+        writeLine("package $packageName")
+        writeLine()
+        writeLine("import dev.sku20.ir.InstanceRegistry")
+        writeLine()
+        writeLine("object $fileName {")
+        writeLine()
+        setIndent(4)
+        writeLine("fun init(registry: InstanceRegistry) {")
     }
 
     private fun writeFooter() {
-        writer.appendLine("}")
-        writer.appendLine("}")
+        setIndent(4)
+        writeLine("}")
+        writeLine()
+        setIndent(0)
+        writeLine("}")
+    }
+
+    private var indent = 0
+    private fun setIndent(n: Int) {
+        indent = n
+    }
+
+    private fun writeLine(s: String = "") {
+        writeIndent()
+        writer.write("")
+        writer.write(s)
+        writer.write("\n")
+    }
+
+    @Suppress("EmptyRange")
+    private fun writeIndent() {
+        for (i in 0 until indent) {
+            writer.write(" ")
+        }
     }
 
 }
