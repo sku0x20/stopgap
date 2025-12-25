f059bbc9030794ef626ea0a4915a2a6bd7cfc69a
siddhant <siddhantkumarupmanyu@gmail.com>
2025-12-26 04:38:48 +0530

    Replace manual writer with `CustomWriter` in `InitCreatorsWriter` for improved code clarity and indentation handling.

--- ir/src/main/kotlin/dev/sku20/ir/ksp/InitCreatorsWriter.kt
+++ ir/src/main/kotlin/dev/sku20/ir/ksp/InitCreatorsWriter.kt
@@ -13,63 +13,44 @@ class InitCreatorsWriter(
     private val packageName: String,
     private val fileName: String
 ) {
-    private val writer = file.bufferedWriter()
+    private val w = CustomWriter(file)
 
     fun write() {
         writeHeader()
         registerCreators()
         writeFooter()
-        writer.close()
+        w.close()
     }
 
     private fun registerCreators() {
         for (symbol in symbols) {
-            setIndent(8)
-            writeLine("registry.registerForType {")
-            setIndent(12)
-            writeLine("${symbol.qualifiedName!!.asString()}(registry)")
-            setIndent(8)
-            writeLine("}")
+            w.setIndent(8)
+            w.writeLine("registry.registerForType {")
+            w.setIndent(12)
+            w.writeLine("${symbol.qualifiedName!!.asString()}(registry)")
+            w.setIndent(8)
+            w.writeLine("}")
         }
     }
 
     private fun writeHeader() {
-        setIndent(0)
-        writeLine("package $packageName")
-        writeLine()
-        writeLine("import dev.sku20.ir.InstanceRegistry")
-        writeLine()
-        writeLine("object $fileName {")
-        writeLine()
-        setIndent(4)
-        writeLine("fun init(registry: InstanceRegistry) {")
+        w.setIndent(0)
+        w.writeLine("package $packageName")
+        w.writeLine()
+        w.writeLine("import dev.sku20.ir.InstanceRegistry")
+        w.writeLine()
+        w.writeLine("object $fileName {")
+        w.writeLine()
+        w.setIndent(4)
+        w.writeLine("fun init(registry: InstanceRegistry) {")
     }
 
     private fun writeFooter() {
-        setIndent(4)
-        writeLine("}")
-        writeLine()
-        setIndent(0)
-        writeLine("}")
-    }
-
-    private var indent = 0
-    private fun setIndent(n: Int) {
-        indent = n
-    }
-
-    private fun writeLine(s: String = "") {
-        writeIndent()
-        writer.write("")
-        writer.write(s)
-        writer.write("\n")
-    }
-
-    @Suppress("EmptyRange")
-    private fun writeIndent() {
-        for (i in 0 until indent) {
-            writer.write(" ")
-        }
+        w.setIndent(4)
+        w.writeLine("}")
+        w.writeLine()
+        w.setIndent(0)
+        w.writeLine("}")
     }
 
 }
