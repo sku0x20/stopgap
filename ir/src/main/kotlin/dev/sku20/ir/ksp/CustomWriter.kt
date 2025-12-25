8fc0fadca1eabd048beb07c3d2ea73e1b8cc2637
siddhant <siddhantkumarupmanyu@gmail.com>
2025-12-26 04:37:19 +0530

    Enhance `CustomWriter` with `withRelativeIndent`, `increaseIndentBy`, and `decreaseIndentBy` for better indentation control.

--- ir/src/main/kotlin/dev/sku20/ir/ksp/CustomWriter.kt
+++ ir/src/main/kotlin/dev/sku20/ir/ksp/CustomWriter.kt
@@ -17,11 +17,25 @@ class CustomWriter(
         writer.write("\n")
     }
 
-    private var indent = 0
+    fun withRelativeIndent(n: Int, block: CustomWriter.() -> Unit) {
+        increaseIndentBy(n)
+        block()
+        decreaseIndentBy(n)
+    }
+
+    fun increaseIndentBy(n: Int) {
+        indent += n
+    }
+
+    fun decreaseIndentBy(n: Int) {
+        indent -= n
+    }
 
     fun setIndent(n: Int) {
         indent = n
     }
+
+    private var indent = 0
 
     @Suppress("EmptyRange")
     private fun writeIndent() {
