bd02c2282c566a279a3ce10952f6e7f6022ae7fa
siddhant <siddhantkumarupmanyu@gmail.com>
2025-12-26 05:30:10 +0530

    Add `EagerlyConfig` and corresponding `EagerlyCreationTest` for eager instance creation testing

---
+++ app/src/intTest/kotlin/dev/sku20/ir/EagerlyCreationTest.kt
@@ -0,0 +1,20 @@
+package dev.sku20.ir
+
+import dev.sku20.ir.generated.initRegistry
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.time.Duration
+
+class EagerlyCreationTest {
+
+    @Test
+    fun eagerly() {
+        val registry = InstanceRegistry()
+        initRegistry(registry)
+        Thread.sleep(10)
+        val eagerly = registry.getInstanceForType<EagerlyConfig.Eagerly>()
+        assertThat(System.nanoTime() - eagerly.creationTime)
+            .isGreaterThan(Duration.ofMillis(10).toNanos())
+    }
+
+}
