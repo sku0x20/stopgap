0c142e9bd9e03eee57ef93d5f0d60f5e0e33243d
siddhant <siddhantkumarupmanyu@gmail.com>
2025-12-28 00:45:32 +0530

    Add `EndpointInitializers` with placeholder `initEndpointPaths` method

---
+++ app/src/main/kotlin/com/example/stopgap/EndpointInitializers.kt
@@ -0,0 +1,11 @@
+package com.example.stopgap
+
+import dev.sku20.ir.InstanceRegistry
+import io.helidon.webserver.http.HttpRouting
+
+fun initEndpointPaths(
+    routes: HttpRouting.Builder,
+    registry: InstanceRegistry
+) {
+    // todo:
+}
