6890ddd19009befa175aff38c0ee717ab5044bcc
siddhant <siddhantkumarupmanyu@gmail.com>
2025-12-28 22:32:25 +0530

    Add `helidon-codegen` module with KSP API dependencies and configuration

---
+++ helidon-codegen/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+}
+
+group = "dev.sku20"
+version = "rolling"
+
+dependencies {
+    implementation(libs.ksp.api)
+}
+
+configurations {
+    val kspApi = libs.ksp.api.get()
+    runtimeElements {
+        exclude(group = kspApi.group, module = kspApi.name)
+    }
+}
