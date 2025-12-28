95c8a2dc5da6f174835f01a18d5e3392d8e6e945
siddhant <siddhantkumarupmanyu@gmail.com>
2025-12-28 23:41:33 +0530

    Annotate `RootEndpoint` with `@Endpoint("/")`

--- app/src/main/kotlin/com/example/stopgap/RootEndpoint.kt
+++ app/src/main/kotlin/com/example/stopgap/RootEndpoint.kt
@@ -1,8 +1,10 @@
 package com.example.stopgap
 
+import dev.sku20.helidon.endpoint.Endpoint
 import io.helidon.webserver.http.ServerRequest
 import io.helidon.webserver.http.ServerResponse
 
+@Endpoint("/")
 class RootEndpoint {
 
     fun ping(req: ServerRequest, res: ServerResponse) {
