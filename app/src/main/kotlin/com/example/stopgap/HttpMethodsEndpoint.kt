3dc998a1ce9198fa0104d91864870eee63e7d1b1
siddhant <siddhantkumarupmanyu@gmail.com>
2025-12-31 17:25:45 +0530

    Extend `HttpMethodsEndpoint` to support all HTTP method annotations.

--- app/src/main/kotlin/com/example/stopgap/HttpMethodsEndpoint.kt
+++ app/src/main/kotlin/com/example/stopgap/HttpMethodsEndpoint.kt
@@ -1,8 +1,6 @@
 package com.example.stopgap
 
-import dev.sku20.helidon.endpoint.Endpoint
-import dev.sku20.helidon.endpoint.Get
-import dev.sku20.helidon.endpoint.Post
+import dev.sku20.helidon.endpoint.*
 import io.helidon.webserver.http.ServerRequest
 import io.helidon.webserver.http.ServerResponse
 
@@ -11,12 +9,47 @@ class HttpMethodsEndpoint {
 
     @Get("/get")
     fun get(req: ServerRequest, res: ServerResponse) {
-        res.send("GET request received");
+        res.send("GET request received")
     }
 
     @Post("/post")
     fun post(req: ServerRequest, res: ServerResponse) {
-        res.send("POST request received");
+        res.send("POST request received")
+    }
+
+    @Delete("/delete")
+    fun delete(req: ServerRequest, res: ServerResponse) {
+        res.send("DELETE request received")
+    }
+
+    @Put("/put")
+    fun put(req: ServerRequest, res: ServerResponse) {
+        res.send("PUT request received")
+    }
+
+    @Patch("/patch")
+    fun patch(req: ServerRequest, res: ServerResponse) {
+        res.send("PATCH request received")
+    }
+
+    @Head("/head")
+    fun head(req: ServerRequest, res: ServerResponse) {
+        res.send("HEAD request received")
+    }
+
+    @Options("/options")
+    fun options(req: ServerRequest, res: ServerResponse) {
+        res.send("OPTIONS request received")
+    }
+
+    @Trace("/trace")
+    fun trace(req: ServerRequest, res: ServerResponse) {
+        res.send("TRACE request received")
+    }
+
+    @Connect("/connect")
+    fun connect(req: ServerRequest, res: ServerResponse) {
+        res.send("CONNECT request received")
     }
 
 }
