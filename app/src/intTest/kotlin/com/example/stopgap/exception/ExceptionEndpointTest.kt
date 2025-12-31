bdfc8a85ded75345ae5955ab88a41ad453a325fc
siddhant <siddhantkumarupmanyu@gmail.com>
2025-12-31 16:56:06 +0530

    Update test URLs in `ExceptionEndpointTest` to include `/exception` path

--- app/src/intTest/kotlin/com/example/stopgap/exception/ExceptionEndpointTest.kt
+++ app/src/intTest/kotlin/com/example/stopgap/exception/ExceptionEndpointTest.kt
@@ -16,13 +16,13 @@ class ExceptionEndpointTest {
 
     @Test
     fun badClient1() {
-        val response = client.get("/bad-client-1").request()
+        val response = client.get("/exception/bad-client-1").request()
         assertThat(response.status()).isEqualTo(Status.BAD_REQUEST_400)
     }
 
     @Test
     fun badClient2() {
-        val response = client.get("/bad-client-2").request()
+        val response = client.get("/exception/bad-client-2").request()
         assertThat(response.status()).isEqualTo(Status.BAD_REQUEST_400)
     }
 
