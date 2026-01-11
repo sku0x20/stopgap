0021fcc0b9258ad27c44f07f013ae31141d22925
siddhant <siddhantkumarupmanyu@gmail.com>
2026-01-12 00:26:35 +0530

    Refactor `Serde` interface to use `ByteArray` for serialization and deserialization instead of `String`.

--- helidon-extensions/src/main/kotlin/dev/sku20/helidon/Serde.kt
+++ helidon-extensions/src/main/kotlin/dev/sku20/helidon/Serde.kt
@@ -3,6 +3,6 @@ package dev.sku20.helidon
 import kotlin.reflect.KClass
 
 interface Serde {
-    fun <T : Any> serialize(obj: T): String
-    fun <T : Any> deserialize(str: String, kClazz: KClass<T>): T
+    fun <T : Any> deserialize(bytes: ByteArray, kClazz: KClass<T>): T
+    fun <T : Any> serialize(obj: T): ByteArray
 }
