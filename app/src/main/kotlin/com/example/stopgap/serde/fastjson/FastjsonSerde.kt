39d7de4926c870ba9feaa26a00d69d178ed4d33e
siddhant <siddhantkumarupmanyu@gmail.com>
2026-04-25 04:12:04 +0530

    Move FastjsonSerde implementation to a dedicated package

---
+++ app/src/main/kotlin/com/example/stopgap/serde/fastjson/FastjsonSerde.kt
@@ -0,0 +1,18 @@
+package com.example.stopgap.serde.fastjson
+
+import dev.sku20.helidon.serde.Serde
+import kotlin.reflect.KClass
+
+// todo: benchmark both Fastjson Serde implementations
+// only supports non-generic types
+// faster than generic one as avoid one if check.
+class FastjsonSerde : Serde {
+
+    override fun <T : Any> deserialize(bytes: ByteArray, kClazz: KClass<T>): T {
+        TODO("Not yet implemented")
+    }
+
+    override fun <T : Any> serialize(obj: T): ByteArray {
+        TODO("Not yet implemented")
+    }
+}
