2f8027b8d3119a3b25ea6b5f4ab5bd80740a6d8c
siddhant <siddhantkumarupmanyu@gmail.com>
2026-04-25 04:26:24 +0530

    Remove outdated comments from FastjsonSerde class

--- app/src/main/kotlin/com/example/stopgap/serde/fastjson/FastjsonSerde.kt
+++ app/src/main/kotlin/com/example/stopgap/serde/fastjson/FastjsonSerde.kt
@@ -3,9 +3,6 @@ package com.example.stopgap.serde.fastjson
 import dev.sku20.helidon.serde.Serde
 import kotlin.reflect.KClass
 
-// todo: benchmark both Fastjson Serde implementations
-// only supports non-generic types
-// faster than generic one as avoid one if check.
 class FastjsonSerde : Serde {
 
     override fun <T : Any> deserialize(bytes: ByteArray, kClazz: KClass<T>): T {
