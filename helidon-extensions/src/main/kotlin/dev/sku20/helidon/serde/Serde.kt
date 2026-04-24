6ff49b83a919ee540a3f721a2ccc65402e8c96c6
siddhant <siddhantkumarupmanyu@gmail.com>
2026-04-25 04:25:54 +0530

    Fix spacing in Serde comment for consistent style

--- helidon-extensions/src/main/kotlin/dev/sku20/helidon/serde/Serde.kt
+++ helidon-extensions/src/main/kotlin/dev/sku20/helidon/serde/Serde.kt
@@ -2,7 +2,7 @@ package dev.sku20.helidon.serde
 
 import kotlin.reflect.KClass
 
-// T : Any ensures non-nullable types only; KClass<T> carries type info lost to JVM erasure.
+// T: Any ensures non-nullable types only; KClass<T> carries type info lost to JVM erasure.
 interface Serde {
     fun <T : Any> deserialize(bytes: ByteArray, kClazz: KClass<T>): T
     fun <T : Any> serialize(obj: T): ByteArray
