f0997e5539033402ee2eb3421e342e06ec0838da
siddhant <siddhantkumarupmanyu@gmail.com>
2026-04-28 03:51:24 +0530

    Refactor import writing to dynamically handle imports in InitializersGenerator

--- helidon-extensions/src/main/kotlin/dev/sku20/helidon/ksp/InitializersGenerator.kt
+++ helidon-extensions/src/main/kotlin/dev/sku20/helidon/ksp/InitializersGenerator.kt
@@ -56,8 +56,7 @@ class InitializersGenerator(
     private fun writeImports() = w.withRelativeIndent {
         writeLine()
         writeLine("import dev.sku20.ir.InstanceRegistry")
-        writeLine("import dev.sku20.helidon.serde.NopSerde")
-        writeLine("import dev.sku20.helidon.serde.Serde")
+        for (import in imports) writeLine("import $import")
         writeLine("import io.helidon.webserver.http.HttpRouting")
         writeLine("import io.helidon.webserver.http.HttpRules")
         writeLine()
