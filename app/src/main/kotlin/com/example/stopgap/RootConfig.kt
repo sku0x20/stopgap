90f53c11a251ab2d90fa3413259fd3ca335a4152
siddhant <siddhantkumarupmanyu@gmail.com>
2026-05-01 18:09:36 +0530

    Add default Serde initialization with FastjsonSerde in RootConfig

--- app/src/main/kotlin/com/example/stopgap/RootConfig.kt
+++ app/src/main/kotlin/com/example/stopgap/RootConfig.kt
@@ -1,6 +1,10 @@
 package com.example.stopgap
 
+import com.example.stopgap.serde.fastjson.FastjsonSerde
+import dev.sku20.helidon.serde.Serde
+import dev.sku20.helidon.serde.SerdeExtras
 import dev.sku20.ir.Creates
+import dev.sku20.ir.Qualifier
 import io.helidon.config.Config
 
 object RootConfig {
@@ -10,6 +14,10 @@ object RootConfig {
         val config = Config.create()
         return config
     }
+
+    @Creates
+    @Qualifier(SerdeExtras.DEFAULT_QUALIFIER)
+    fun defaultSerde(): Serde = FastjsonSerde()
 
     @Creates
     fun rootEndpoint(): RootEndpoint {
