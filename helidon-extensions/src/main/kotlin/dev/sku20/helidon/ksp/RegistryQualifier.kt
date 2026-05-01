0be7db4c7ab25e61f9ec888293c07090df25f7f9
siddhant <siddhantkumarupmanyu@gmail.com>
2026-05-01 16:57:37 +0530

    Update RegistryQualifier to target value parameters instead of functions

--- helidon-extensions/src/main/kotlin/dev/sku20/helidon/ksp/RegistryQualifier.kt
+++ helidon-extensions/src/main/kotlin/dev/sku20/helidon/ksp/RegistryQualifier.kt
@@ -2,7 +2,7 @@ package dev.sku20.helidon.ksp
 
 import kotlin.reflect.KClass
 
-@Target(AnnotationTarget.FUNCTION)
+@Target(AnnotationTarget.VALUE_PARAMETER)
 @Retention(AnnotationRetention.SOURCE)
 annotation class RegistryQualifier(
     val qualifier: String = "",
