fde6b74f5e70d33523346783058b522b9ae6a959
siddhant <siddhantkumarupmanyu@gmail.com>
2026-06-22 04:53:17 +0530

    Remove unused `DEFAULT_QUALIFIER` constant from `SerdeExtras`.

--- helidon-extensions/src/main/kotlin/dev/sku20/helidon/serde/Extras.kt
+++ helidon-extensions/src/main/kotlin/dev/sku20/helidon/serde/Extras.kt
@@ -1,6 +1,5 @@
 package dev.sku20.helidon.serde
 
 object SerdeExtras {
-    const val DEFAULT_QUALIFIER = "dev.sku20.helidon.serde.default"
     const val DEFAULT_CATALOG_QUALIFIER = "dev.sku20.helidon.serde.catalog.default"
 }
