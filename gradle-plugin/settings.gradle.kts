0e50af24316a17f89ef0a8a1484d30005cee9c57
siddhant <siddhantkumarupmanyu@gmail.com>
2026-07-02 03:57:39 +0530

    Add repositories to dependencyResolutionManagement in gradle-plugin settings

--- gradle-plugin/settings.gradle.kts
+++ gradle-plugin/settings.gradle.kts
@@ -1,6 +1,12 @@
+@file:Suppress("UnstableApiUsage")
+
 rootProject.name = "gradle-plugin"
 
 dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
     versionCatalogs {
         create("libs") {
             from(files("../gradle/libs.versions.toml"))
